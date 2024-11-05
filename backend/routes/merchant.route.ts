19aa72785a83ab58784dbcbaf85c88ae56053119
Camden Scholl <89877319+c-l-scholl@users.noreply.github.com>
2024-11-05 10:43:31 -0600

    changed backend routing to controller:

--- backend/routes/merchant.route.ts
+++ backend/routes/merchant.route.ts
@@ -7,59 +7,28 @@ import {
 } from "../controller/merchant.controller.js";
 
 const router = express.Router();
-
 /**
  * Get merchant details.
- *
- * @route GET api/merchants/
- * @param {Request} req - Express request object
- * @param {Response} res - Express response object
- * @param {NextFunction} next - Express next middleware function
- * @throws {BadRequestError} If the limit is not a number
  */
 router.get("/", getMerchants);
 
 /**
  * Get a specific merchant
- *
- * @route GET api/merchants/:id
- * @param {Request} req - Express request object
- * @param {Response} res - Express response object
- * @param {NextFunction} next - Express next middleware function
- * @throws {BadRequestError} If the merchant with the specified ID is not found
  */
 router.get("/:id", getMerchantByID);
 
 /**
  * Create merchant details.
- *
- * @route POST api/merchants
- * @param {Request} req - Express request object
- * @param {Response} res - Express response object
- * @param {NextFunction} next - Express next middleware function
- * @throws {BadRequestError} If the merchant with the specified ID is not found
  */
 router.post("/", updateMerchant);
 
 /**
  * Update merchant details.
- *
- * @route PUT api/merchants/:id
- * @param {Request} req - Express request object
- * @param {Response} res - Express response object
- * @param {NextFunction} next - Express next middleware function
- * @throws {BadRequestError} If the merchant with the specified ID is not found
  */
 router.put("/:id", updateMerchant);
 
 /**
  * Delete a merchant.
- *
- * @route DELETE api/merchants/:id
- * @param {Request} req - Express request object
- * @param {Response} res - Express response object
- * @param {NextFunction} next - Express next middleware function
- * @throws {BadRequestError} If the merchant with the specified ID is not found
  */
 router.delete("/:id", deleteMerchant);
 
--- frontend/src/routes/messengers-page.tsx
+++ frontend/src/routes/messengers-page.tsx
@@ -2,7 +2,9 @@
 const MessengersPage = () => {
 
 	return (
-		<></>
+		<>
+			
+		</>
 	)
 }
 
