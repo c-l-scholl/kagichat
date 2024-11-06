8111003b277abfeac34477fc3dc71f9fe49dab78
c-l-scholl <cscholl@macalester.edu>
2024-11-06 15:39:38 -0600

    fixed auth context error with tsx file

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, ReactNode, useContext, useReducer } from "react";
+import { createContext, ReactNode, useContext, useReducer } from "react";
 
 type AuthUser = {
 	uid: string;
